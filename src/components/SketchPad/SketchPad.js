a5abac5cc9e42325d2a2a0c269cb482eb1a5d118
Oliver Ballon <oliverballon1@gmail.com>
2020-09-16 11:01:44 +0100

    layout containers

--- src/components/SketchPad/SketchPad.js
+++ src/components/SketchPad/SketchPad.js
@@ -1,11 +1,10 @@
-import React, { useRef, useEffect, useState } from 'react';
-import Button from '../Button/Button';
+import React, { useRef, useEffect, useState } from "react";
+import Button from "../Button/Button";
 
 const SketchPad = ({ round, handleSave }) => {
-
-  const canvasRef = useRef(null)
-  const contextRef = useRef(null)
-  const [isDrawing, setIsDrawing] = useState(false)
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+  const [isDrawing, setIsDrawing] = useState(false);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -14,56 +13,60 @@ const SketchPad = ({ round, handleSave }) => {
     canvas.style.width = `${500}px`;
     canvas.style.height = `${500}px`;
 
-    const context = canvas.getContext("2d")
-    context.scale(2, 2)
-    context.lineCap = "round"
-    context.strokeStyle = `${round === 1 ? "blue" : "green"}`
-    context.lineWidth = 5
+    const context = canvas.getContext("2d");
+    context.scale(2, 2);
+    context.lineCap = "round";
+    context.strokeStyle = `${round === 1 ? "blue" : "green"}`;
+    context.lineWidth = 5;
     contextRef.current = context;
-  }, [round])
+  }, [round]);
 
   const startDrawing = ({ nativeEvent }) => {
     const { offsetX, offsetY } = nativeEvent;
-    contextRef.current.beginPath()
-    contextRef.current.moveTo(offsetX, offsetY)
-    setIsDrawing(true)
-  }
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(offsetX, offsetY);
+    setIsDrawing(true);
+  };
 
   const finishDrawing = () => {
-    contextRef.current.closePath()
-    setIsDrawing(false)
-  }
+    contextRef.current.closePath();
+    setIsDrawing(false);
+  };
 
   const draw = ({ nativeEvent }) => {
     if (!isDrawing) {
-      return
+      return;
     }
     const { offsetX, offsetY } = nativeEvent;
-    contextRef.current.lineTo(offsetX, offsetY)
-    contextRef.current.stroke()
-  }
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const canvas = canvasRef.current;
     const dataURL = canvas.toDataURL();
     handleSave(round, dataURL);
-  }
+  };
 
   return (
     <>
-      <canvas
-        className="border border-primary"
-        onMouseDown={startDrawing}
-        onMouseUp={finishDrawing}
-        onMouseMove={draw}
-        ref={canvasRef}
-      />
-      <Button 
-        buttonClass="container btn btn-success"
-        handleClick={ (e) => handleSubmit(e) }
-        label="Submit"
-      />
+      <div className="container-global">
+        <div className="container-sketchpad">
+          <canvas
+            className="border border-primary"
+            onMouseDown={startDrawing}
+            onMouseUp={finishDrawing}
+            onMouseMove={draw}
+            ref={canvasRef}
+          />
+          <Button
+            buttonClass="submit"
+            handleClick={(e) => handleSubmit(e)}
+            label="Submit"
+          />
+        </div>
+      </div>
     </>
   );
 };
