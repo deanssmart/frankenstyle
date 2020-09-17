14c9fb48d7adcb9a39c2da51955d9bfafc0c5203
deanssmart <deanssmart@gmail.com>
2020-09-17 15:57:54 +0100

    Updates

---
+++ precache-manifest.e38ebb4cb95ee04e7ed931aa71931f8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bea933c16aca252bd962828f6d9f5ea7",
+    "url": "/GroupProject_Front-End/index.html"
+  },
+  {
+    "revision": "9e684c329107fd235a20",
+    "url": "/GroupProject_Front-End/static/css/main.83e85688.chunk.css"
+  },
+  {
+    "revision": "f68fa52194c934ae7947",
+    "url": "/GroupProject_Front-End/static/js/2.909ec38b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GroupProject_Front-End/static/js/2.909ec38b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e684c329107fd235a20",
+    "url": "/GroupProject_Front-End/static/js/main.c211b63d.chunk.js"
+  },
+  {
+    "revision": "bbf1b4fd41179b6ac00e",
+    "url": "/GroupProject_Front-End/static/js/runtime-main.9a40ac4d.js"
+  },
+  {
+    "revision": "92e5c3c71c8f40a19900edcd273a5fcf",
+    "url": "/GroupProject_Front-End/static/media/Run.92e5c3c7.woff"
+  }
+]);
